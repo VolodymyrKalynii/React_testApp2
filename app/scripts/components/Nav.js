a90377e84e88161c91937da8a3e9172eb391458f
VolodymyrKalynii <v.kalynii@gmail.com>
2019-02-21 15:32:34 +0200

    Change links, rebuild

--- app/scripts/components/Nav.js
+++ app/scripts/components/Nav.js
@@ -6,8 +6,8 @@ export default class Nav extends React.Component{
         return (
             <div className='nav'>
                 <div className='container'>
-                    <NavLink className='nav__link' exact activeClassName='nav__link-active' to='/'>Home</NavLink>
-                    <NavLink className='nav__link' activeClassName='nav__link-active' to='/bookmarks'>Bookmarks</NavLink>
+                    <NavLink className='nav__link' exact activeClassName='nav__link-active' to='/React_testApp2/dist/'>Home</NavLink>
+                    <NavLink className='nav__link' activeClassName='nav__link-active' to='/React_testApp2/dist/bookmarks'>Bookmarks</NavLink>
                 </div>
             </div>
         );
