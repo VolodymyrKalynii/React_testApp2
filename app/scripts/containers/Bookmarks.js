612f58e616117219c823299b997d53562e0bf66a
VolodymyrKalynii <v.kalynii@gmail.com>
2019-03-07 10:52:50 +0200

    Refactor connect code

--- app/scripts/containers/Bookmarks.js
+++ app/scripts/containers/Bookmarks.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import connect from 'react-redux/es/connect/connect';
+import {connect} from 'react-redux';
 
 import {bookmarksActions} from '../redux/actions';
 import MoviesList from '../components/MoviesList';
--- app/scripts/containers/MovieWrapper.js
+++ app/scripts/containers/MovieWrapper.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import connect from 'react-redux/es/connect/connect';
+import {connect} from 'react-redux';
 
 import {movieActions} from '../redux/actions/index';
 import Loader from '../components/Loader';
